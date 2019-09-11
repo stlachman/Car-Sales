114a3218d530d892c2157f623c4c6b67f5208ecf
Seth Lachman <lachmanseth17@gmail.com>
2019-09-10 23:29:57 -0400

    Added conditional to prevent duplicate features.

--- src/components/AddedFeature.js
+++ src/components/AddedFeature.js
@@ -3,7 +3,6 @@ import React from "react";
 const AddedFeature = props => {
   return (
     <li>
-      {/* Add an onClick to run a function to remove a feature */}
       <button
         onClick={e => props.removeFeature(e, props.feature)}
         className="button"
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,25 +4,27 @@ import { ADD_ITEM, REMOVE_ITEM } from "../actions/actions";
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case ADD_ITEM:
-      return {
-        ...state,
-        additionalPrice: (state.additionalPrice += action.payload.price),
-        car: {
-          ...state.car,
-          features: [...state.car.features, action.payload]
-        }
-      };
+      if (!state.car.features.includes(action.payload)) {
+        return {
+          ...state,
+          additionalPrice: (state.additionalPrice += action.payload.price),
+          car: {
+            ...state.car,
+            features: [...state.car.features, action.payload]
+          }
+        };
+      } else {
+        return state;
+      }
     case REMOVE_ITEM:
       return {
         ...state,
         additionalPrice: (state.additionalPrice -= action.payload.price),
         car: {
           ...state.car,
-          features: [
-            ...state.car.features.filter(
-              feature => feature.id !== action.payload.id
-            )
-          ]
+          features: state.car.features.filter(
+            feature => feature.id !== action.payload.id
+          )
         }
       };
     default:
